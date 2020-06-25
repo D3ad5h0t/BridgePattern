0f0e9f30ce321b0187868381364cb9690aa88d9a
D3ad5h0t <alexgl007@mail.ru>
2020-06-25 16:47:01 +1000

    basic bridge

---
+++ Bridge/Program.cs
@@ -0,0 +1,19 @@
+﻿using Bridge.Renderers;
+using Bridge.Shapes;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var raster = new RasterRenderer();
+            var vector = new VectorRenderer();
+            var circle = new Circle(raster, 5);
+            
+            circle.Draw();
+            circle.Resize(2);
+            circle.Draw();
+        }
+    }
+}
---
+++ Bridge/Shapes/Shape.cs
@@ -0,0 +1,17 @@
+﻿using Bridge.Renderers;
+
+namespace Bridge.Shapes
+{
+    public abstract class Shape
+    {
+        protected IRenderer _renderer;
+
+        protected Shape(IRenderer renderer)
+        {
+            _renderer = renderer;
+        }
+
+        public abstract void Draw();
+        public abstract void Resize(float factor);
+    }
+}
